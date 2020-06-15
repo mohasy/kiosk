aa18032c42769c889c0328a7777db9420e2b4d81
mohasy <304182@naver.com>
2020-06-15 19:04:25 +0900

    test code

---
+++ src/test/java/com/grammer/kiosk/repository/StoreRepositoryTests.java
@@ -0,0 +1,21 @@
+package com.grammer.kiosk.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class StoreRepositoryTests {
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Test
+    public void insertDummies(){
+        
+
+    }
+
+    
+    
+}
